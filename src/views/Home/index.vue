085aec8ce075fd857f0b8e011f174907f9088126
YGT-cxy <1159514579@qq.com>
2019-06-14 21:14:10 +0800

    重新规划项目的目录

---
+++ src/views/Home/index.vue
@@ -0,0 +1,19 @@
+<!-- 根目录 -->
+<template>
+  <div>
+    根目录
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
