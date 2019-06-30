70546d820e4e123c3e9b992a39e33b9df9aa2383
YGT-cxy <1159514579@qq.com>
2019-06-30 20:16:37 +0800

    'store的mutations和actions的使用'

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -2,9 +2,21 @@ const state = {
   appName: 'vue开发的后台管理系统'
 }
 
-const mutations = {}
+const mutations = {
+  SET_APP_NAME: (state, params) => {
+    // console.log(state)
+    // console.log(params)
+    state.appName = params
+  }
+}
 
-const actions = {}
+const actions = {
+  setAppName: ({ commit }, params) => {
+    // console.log(params)
+    // console.log(commit)
+    commit('SET_APP_NAME', params)
+  }
+}
 
 export default {
   namespaced: true, // 开启命名空间
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -10,6 +10,18 @@
       <button @click="navtar">跳转到parent</button>
       <button @click="replace">替换到name</button>
     </div>
+    <div>
+      <p>appName: {{ $store.state.app.appName }}</p>
+      <button type="button" @click="setStore">设置store</button>
+    </div>
+    <div>
+      <p v-for="(item, index) of todoList" :key="index">{{ item }}</p>
+      <button type="button" @click="register">动态注册store模块</button>
+    </div>
+    <div>
+      <p v-for="(item, index) of userTodoList" :key="index">{{ item }}</p>
+      <button type="button" @click="register2">动态注册store二级子模块</button>
+    </div>
   </div>
 </template>
 
@@ -20,14 +32,22 @@ export default {
     return {
     }
   },
+  computed: {
+    todoList() {
+      return this.$store.state.todo ? this.$store.state.todo.todoList : []
+    },
+    userTodoList() {
+      return this.$store.state.user.todo ? this.$store.state.user.todo.todoList : []
+    }
+  },
   mounted() {
-    console.log(this.$store)
-    console.log(this.$store.getters.app)
-    console.log(this.$route.params)
+    // console.log(this.$store)
+    // console.log(this.$store.getters.app)
+    // console.log(this.$route.params)
   },
   methods: {
     back() {
-      console.log(this.$router)
+      // console.log(this.$router)
       this.$router.back()
     },
     navtar() {
@@ -45,6 +65,34 @@ export default {
           name: 1234
         }
       })
+    },
+    // 设置store
+    setStore() {
+      this.$store.dispatch('app/setAppName', '后台')
+    },
+    // 动态注册模块
+    register() {
+      // console.log(this.$store)
+      this.$store.registerModule('todo', {
+        state: {
+          todoList: [
+            '学习mutations',
+            '学习actions'
+          ]
+        }
+      })
+    },
+    // 动态注册二级子模块
+    register2() {
+      this.$store.registerModule(['user', 'todo'], {
+        state: {
+          todoList: [
+            '学习mutations',
+            '学习actions'
+          ]
+        }
+      })
+      // console.log(this.$store.state)
     }
   }
 }
